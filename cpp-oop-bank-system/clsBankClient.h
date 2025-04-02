9b7fc4ce062e1054f609a78af366bda7b5c749e3
Chiheb abiza <142681693+chihebabiza@users.noreply.github.com>
2025-04-02 10:26:17 +0100

    add show balances function

--- cpp-oop-bank-system/clsBankClient.h
+++ cpp-oop-bank-system/clsBankClient.h
@@ -369,6 +369,22 @@ public:
         return _LoadClientsDataFromFile();
     }
 
+    static float GetTotalBalances()
+    {
+        vector <clsBankClient> vClients = clsBankClient::GetClientsList();
+
+        double TotalBalances = 0;
+
+        for (clsBankClient Client : vClients)
+        {
+
+            TotalBalances += Client.AccountBalance;
+        }
+
+        return TotalBalances;
+
+    }
+
 
 };
 
