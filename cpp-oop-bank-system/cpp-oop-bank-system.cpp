2b9101321f7b11d4291b23c99f11ec2af2e777f0
Chiheb abiza <142681693+chihebabiza@users.noreply.github.com>
2025-04-02 09:24:25 +0100

    initilaze the project

--- cpp-oop-bank-system/cpp-oop-bank-system.cpp
+++ cpp-oop-bank-system/cpp-oop-bank-system.cpp
@@ -1,20 +1,76 @@
-// cpp-oop-bank-system.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
 
-int main()
+void ReadClientInfo(clsBankClient& Client)
 {
-    std::cout << "Hello World!\n";
+    cout << "\nEnter FirstName: ";
+    Client.FirstName = clsInputValidate::ReadString();
+
+    cout << "\nEnter LastName: ";
+    Client.LastName = clsInputValidate::ReadString();
+
+    cout << "\nEnter Email: ";
+    Client.Email = clsInputValidate::ReadString();
+
+    cout << "\nEnter Phone: ";
+    Client.Phone = clsInputValidate::ReadString();
+
+    cout << "\nEnter PinCode: ";
+    Client.PinCode = clsInputValidate::ReadString();
+
+    cout << "\nEnter Account Balance: ";
+    Client.AccountBalance = clsInputValidate::ReadFloatNumber();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void UpdateClient()
+{
+    string AccountNumber = "";
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    cout << "\nPlease Enter client Account Number: ";
+    AccountNumber = clsInputValidate::ReadString();
+
+    while (!clsBankClient::IsClientExist(AccountNumber))
+    {
+        cout << "\nAccount number is not found, choose another one: ";
+        AccountNumber = clsInputValidate::ReadString();
+    }
+
+    clsBankClient Client1 = clsBankClient::Find(AccountNumber);
+    Client1.Print();
+
+    cout << "\n\nUpdate Client Info:";
+    cout << "\n____________________\n";
+
+
+    ReadClientInfo(Client1);
+
+    clsBankClient::enSaveResults SaveResult;
+
+    SaveResult = Client1.Save();
+
+    switch (SaveResult)
+    {
+    case  clsBankClient::enSaveResults::svSucceeded:
+    {
+        cout << "\nAccount Updated Successfully :-)\n";
+        Client1.Print();
+        break;
+    }
+    case clsBankClient::enSaveResults::svFaildEmptyObject:
+    {
+        cout << "\nError account was not saved because it's Empty";
+        break;
+
+    }
+
+    }
+}
+
+int main()
+
+{
+    UpdateClient();
+    system("pause>0");
+    return 0;
+}
