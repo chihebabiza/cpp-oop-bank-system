bb7f12993f62c9ee675bcc3f7c902d683319a871
Chiheb abiza <142681693+chihebabiza@users.noreply.github.com>
2025-04-19 11:38:06 +0100

    add Current date and current user in all screens

--- cpp-oop-bank-system/clsScreen.h
+++ cpp-oop-bank-system/clsScreen.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "clsUser.h"
 #include "Global.h"
+#include "clsDate.h"
 
 using namespace std;
 
@@ -17,6 +18,8 @@ protected:
             cout << "\n\t\t\t\t\t  " << SubTitle;
         }
         cout << "\n\t\t\t\t\t______________________________________\n\n";
+        cout << "\n\t\t\t\t\t  User : "<<CurrentUser.UserName;
+        cout << "\n\t\t\t\t\t  Date : " << clsDate::DateToString(clsDate())<< "\n\n";
     }
 
     static bool CheckAccessRights(clsUser::enPermissions Permission)
