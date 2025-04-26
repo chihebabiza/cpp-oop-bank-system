4373c3b730a14c3fba0902817a21e56b6766abcd
Chiheb abiza <142681693+chihebabiza@users.noreply.github.com>
2025-04-26 05:58:17 +0100

    updates

--- cpp-oop-bank-system/clsAddNewUserScreen.h
+++ cpp-oop-bank-system/clsAddNewUserScreen.h
@@ -113,6 +113,12 @@ private:
         {
             Permissions += clsUser::enPermissions::pManageUsers;
         }
+		cout << "\nLogin Register? y/n? ";
+		cin >> Answer;
+		if (Answer == 'y' || Answer == 'Y')
+		{
+			Permissions += clsUser::enPermissions::pLoginRegister;
+		}
 
         return Permissions;
 
